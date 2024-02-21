6d50879a811c783400585114762ef630227e2d54
Lucas Braud <lucas.braud@cern.ch>
2024-02-21 16:12:06 +0100

    Latest rough weights

--- deploy.py
+++ deploy.py
@@ -2,6 +2,6 @@
 from agent2 import Agent
 
 #Go1 = Agent(path='weights/asym_full_model_10000.pt')
-Go1 = Agent(path='weights/model_50000_asym3.pt')
+Go1 = Agent(path='weights/model_100000_asym.pt')
 
 Go1.init_pose()

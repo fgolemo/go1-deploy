20b9912522e601772f5144bd0d31cfbfeb393d7a
Lucas Braud <lucas.braud@cern.ch>
2024-03-01 15:52:57 +0100

    keep rough 150k

--- deploy.py
+++ deploy.py
@@ -2,6 +2,6 @@
 from agent2 import Agent
 
 #Go1 = Agent(path='weights/asym_full_model_10000.pt')
-Go1 = Agent(path='weights/rough_43650_P.pt')
+Go1 = Agent(path='weights/rough_150000.pt')
 
 Go1.init_pose()

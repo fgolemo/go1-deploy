710b7621f06d1e84007410b163fc6ce8a92ca4fa
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2024-06-16 22:20:10 -0400

    MRSS 2024

--- deploy.py
+++ deploy.py
@@ -1,7 +1,21 @@
-#from agent import Agent
-from agent2 import Agent
+# from agent import Agent
+# from agent2 import Agent
+from agent3 import Agent
 
-#Go1 = Agent(path='weights/asym_full_model_10000.pt')
-Go1 = Agent(path='weights/rough_150000.pt')
+from argparse import ArgumentParser
 
-Go1.init_pose()
+
+def main(args):
+    go_1 = Agent(path=args.model, server=args.server, port=args.port, joystick=not args.no_joystick)
+    go_1.init_pose()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('--server', action='store_true', help='use a command server')
+    parser.add_argument('--no-joystick', dest='no_joystick', action='store_true', help='disable joystick')
+    parser.add_argument('-p', '--port', type=int, default=9292, help='port to receive commands from')
+    parser.add_argument('-m', '--model', type=str, default='weights/rough_150000.pt')  # 'weights/asym_full_model_10000.pt'
+    arguments = parser.parse_args()
+
+    main(arguments)
